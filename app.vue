5758b9d32321461211a14c70c4e59204613d8e33
JosefinaTrojkarova <trojkarjosef@gmail.com>
2024-10-27 13:14:30 +0100

    Initial testing #1

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Welcome to this site</h1>
+    <p>If you're here you're either very special to me or have made a mistake.</p>
   </div>
 </template>
