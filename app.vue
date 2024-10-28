ccccd2f82322803ebe381625bbe05faf20c31c64
JosefinaTrojkarova <trojkarjosef@gmail.com>
2024-10-28 11:00:36 +0100

    Test change

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <h1>Welcome to this site</h1>
-    <p>If you're here you're either very special to me or have made a mistake.</p>
+    <p>If you're here you're either very special to me or have made a mistake...</p>
   </div>
 </template>
